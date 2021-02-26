d2e116013fb9abe391c5e0d35b1389f5495d3e67
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2021-02-26 18:30:32 +0000

    Lint

--- pkg/middleware/scope.go
+++ pkg/middleware/scope.go
@@ -11,7 +11,7 @@ func NewScope(reverseProxy bool) alice.Constructor {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			scope := &middlewareapi.RequestScope{
-				ReverseProxy: reverseProxy,
+				ReverseProxy:    reverseProxy,
 				SessionRequired: true,
 			}
 			req = middlewareapi.AddRequestScope(req, scope)
